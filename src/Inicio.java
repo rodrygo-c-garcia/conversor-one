910f424ec5f70d819f77d1757786cb33b4db059a
Rodrigo Colque Garcia <colqueariel23@gmail.com>
2023-08-15 16:31:32 -0400

    feat: add panelMain on constructor

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 
 public class Inicio extends JFrame {
+    private JPanel panelMain;
+    private JLabel labelTitle;
+
     public Inicio() {
         super("Conversor ONE");
         setSize(900, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
+        add(panelMain);
     }
 }
